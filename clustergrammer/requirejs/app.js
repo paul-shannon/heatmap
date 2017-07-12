14f48920de2b2096a8705bb9bf4fd4815b2d673f
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-07-12 11:57:34 -0700

    demo data (3x3 matrix) now loaded from tinyData.js, not inline

--- clustergrammer/requirejs/app.js
+++ clustergrammer/requirejs/app.js
@@ -10,94 +10,9 @@ require.config({
     });
 
 require(['underscore', 'jquery',  'clustergrammer'], function (_, $, clustergrammer) {
+
     window.cwg = clustergrammer;
-    network_data =
-        {"views":
-         [{"N_row_sum": "all",
-           "dist": "cos",
-           "nodes":
-             {"col_nodes": [{"ini": 3, "rank": 2, "name": "s01_120405", "clust": 1,
-                             "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 2},
-                            {"ini": 2, "rank": 1, "name": "s02_120405", "clust": 0,
-                             "group": [3.0, 3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 0},
-                            {"ini": 1, "rank": 0, "name": "s03_120405", "clust": 2,
-                             "group": [2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 1}],
-              "row_nodes": [{"ini": 2, "rank": 0, "name": "HLTF", "clust": 0,
-                             "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 0},
-                            {"ini": 1, "rank": 1, "name": "POU2F1", "clust": 1,
-                             "group": [2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0], "rankvar": 1}]}},
-            {"dist": "cos", "N_row_var": "all", "nodes":
-           {"col_nodes": [{"ini": 3, "rank": 2, "name": "s01_120405", "clust": 1,
-                           "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 2},
-                          {"ini": 2, "rank": 1, "name": "s02_120405", "clust": 0,
-                           "group": [3.0, 3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 0},
-                          {"ini": 1, "rank": 0, "name": "s03_120405", "clust": 2,
-                           "group": [2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 1}],
-            "row_nodes": [{"ini": 2, "rank": 0, "name": "HLTF", "clust": 0,
-                           "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 0},
-                          {"ini": 1, "rank": 1, "name": "POU2F1", "clust": 1,
-                           "group": [2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0], "rankvar": 1}]}}],
-         "col_nodes": [{"ini": 3, "rank": 2, "name": "s01_120405", "clust": 1,
-                        "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 2},
-                       {"ini": 2, "rank": 1, "name": "s02_120405", "clust": 0,
-                        "group": [3.0, 3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 0},
-                       {"ini": 1, "rank": 0, "name": "s03_120405", "clust": 2,
-                        "group": [2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 1}],
-         "links": [{"target": 0, "value": 5.591991, "source": 0},
-                   {"target": 1, "value": 11.939007, "source": 0},
-                   {"target": 2, "value": 7.738552, "source": 0},
-                   {"target": 0, "value": 31.060965999999997, "source": 1},
-                   {"target": 1, "value": 18.00348, "source": 1},
-                   {"target": 2, "value": 21.577569, "source": 1}],
-         "row_nodes": [{"ini": 2, "rank": 0, "name": "HLTF", "clust": 0,
-                        "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], "rankvar": 0},
-                       {"ini": 1, "rank": 1, "name": "POU2F1", "clust": 1,
-                        "group": [2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0], "rankvar": 1}]};
-
-
-    var tinyData = {"row_nodes": [{"name": "Row: R1", "ini": 3, "clust": 1, "rank": 0,
-          "rankvar": 0, "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0]}, {"name": "Row: R2", "ini": 2, "clust": 0, "rank": 2,
-          "rankvar": 2, "group": [3.0, 3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0,
-          1.0, 1.0]}, {"name": "Row: R3", "ini": 1, "clust": 2, "rank": 1,
-          "rankvar": 1, "group": [2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0]}], "col_nodes": [{"name": "Col: C1", "ini": 3, "clust": 1,
-          "rank": 1, "rankvar": 1, "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0, 1.0, 1.0]}, {"name": "Col: C2", "ini": 2, "clust": 0,
-          "rank": 0, "rankvar": 0, "group": [3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,
-          2.0, 2.0, 2.0, 1.0]}, {"name": "Col: C3", "ini": 1, "clust": 2,
-          "rank": 2, "rankvar": 2, "group": [2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0, 1.0, 1.0]}], "links": [], "mat": [[0.0, -0.1, 1.0], [3.0,
-          0.0, 8.0], [0.2, 0.1, 2.5]], "cat_colors": {"row": {}, "col": {}},
-          "views": [{"N_row_sum": "all", "dist": "cos", "nodes": {"row_nodes":
-          [{"name": "Row: R1", "ini": 3, "clust": 1, "rank": 0, "rankvar": 0,
-          "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]},
-          {"name": "Row: R2", "ini": 2, "clust": 0, "rank": 2, "rankvar": 2,
-          "group": [3.0, 3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0]},
-          {"name": "Row: R3", "ini": 1, "clust": 2, "rank": 1, "rankvar": 1,
-          "group": [2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]}],
-          "col_nodes": [{"name": "Col: C1", "ini": 3, "clust": 1, "rank": 1,
-          "rankvar": 1, "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0]}, {"name": "Col: C2", "ini": 2, "clust": 0, "rank": 0,
-          "rankvar": 0, "group": [3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,
-          2.0, 1.0]}, {"name": "Col: C3", "ini": 1, "clust": 2, "rank": 2,
-          "rankvar": 2, "group": [2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0]}]}}, {"N_row_var": "all", "dist": "cos", "nodes":
-          {"row_nodes": [{"name": "Row: R1", "ini": 3, "clust": 1, "rank": 0,
-          "rankvar": 0, "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0]}, {"name": "Row: R2", "ini": 2, "clust": 0, "rank": 2,
-          "rankvar": 2, "group": [3.0, 3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0,
-          1.0, 1.0]}, {"name": "Row: R3", "ini": 1, "clust": 2, "rank": 1,
-          "rankvar": 1, "group": [2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0]}], "col_nodes": [{"name": "Col: C1", "ini": 3, "clust": 1,
-          "rank": 1, "rankvar": 1, "group": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0, 1.0, 1.0]}, {"name": "Col: C2", "ini": 2, "clust": 0,
-          "rank": 0, "rankvar": 0, "group": [3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,
-          2.0, 2.0, 2.0, 1.0]}, {"name": "Col: C3", "ini": 1, "clust": 2,
-          "rank": 2, "rankvar": 2, "group": [2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-          1.0, 1.0, 1.0, 1.0]}]}}]}
-
-    make_clust(tinyData);
+    make_clust(demo3x3);
 
     var about_string = 'Zoom, scroll, and click buttons to interact with the clustergram. <a href="http://amp.pharm.mssm.edu/clustergrammer/help"> <i class="fa fa-question-circle" aria-hidden="true"></i> </a>';
     function make_clust(network_data){
