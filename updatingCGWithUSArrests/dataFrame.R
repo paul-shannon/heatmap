0f60dcc3cc5d8e4e7ba87373fdd5af43ca887b6a
oshah97 <oshah97@outlook.com>
2017-07-24 11:35:35 -0700

    more small changes

--- updatingCGWithUSArrests/dataFrame.R
+++ updatingCGWithUSArrests/dataFrame.R
@@ -52,8 +52,6 @@ dataFrame <<- function() {
 
    dataFrameRaw <<- list(row_nodes=row_nodes, col_nodes=col_nodes, mat=mat)
    dataFramejson <<- toJSON(dataFrameRaw)
-
-   return(dataFrameRaw) #to check if the RUnit test find true that matrix is returned
    
    }#dataFrame
 #--------------------------------------------------------------------------------
