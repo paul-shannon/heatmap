4ac27d987c61414ee2699d3445625212ad3a20f2
oshah97 <oshah97@outlook.com>
2017-07-24 16:53:14 -0700

    addition of large USArrests data in clustergrammer

--- clustergrammer/requirejs/app.js
+++ clustergrammer/requirejs/app.js
@@ -12,7 +12,7 @@ require.config({
 require(['underscore', 'jquery',  'clustergrammer'], function (_, $, clustergrammer) {
 
     window.cwg = clustergrammer;
-    make_clust(demo3x3);
+    make_clust(demoUSArrests); // options: demo3x3, demoUSArrestsSmall (is 3x4), demoUSArrests (full data set) CHANGE THIS TO CHANGE CLUSTERGRAMMER
 
     var about_string = 'Zoom, scroll, and click buttons to interact with the clustergram. <a href="http://amp.pharm.mssm.edu/clustergrammer/help"> <i class="fa fa-question-circle" aria-hidden="true"></i> </a>';
     function make_clust(network_data){
--- updatingCGWithUSArrests/dataFrame.R
+++ updatingCGWithUSArrests/dataFrame.R
@@ -17,10 +17,11 @@ dataFrame <<- function() {
    rowname <<- hc1$labels
    rowclust <<- hc1$order
    rowrank <<- hc1$order
+   rev_hc1 <<- rev(hc1$order) 
    rowgroup <<- list()
    rowFill <<- function() {
                   for(i in 1:nrow(treeMtx)) {
-                     rowgroup[[i]] <<- treeMtx[i,]
+                     rowgroup[[i]] <<- rev(treeMtx[rev_hc1[i],])
                      }
                   return(I(rowgroup))
                   }
@@ -34,10 +35,11 @@ dataFrame <<- function() {
    colname <<- hc2$labels
    colclust <<- hc2$order
    colrank <<- hc2$order
+   rev_hc2 <<- rev(hc2$order) 
    colgroup <<- list()
    colFill <<- function() {
                   for(i in 1:nrow(treeMtx2)) {
-                     colgroup[[i]] <<- treeMtx2[i,]
+                     colgroup[[i]] <<- rev(treeMtx2[rev_hc2[i],])
                      }
                   return(I(colgroup))
                   }
@@ -57,4 +59,60 @@ dataFrame <<- function() {
    #return(dataFramejson) 
    
    }#dataFrame
+#--------------------------------------------------------------------------------
+dataFrameBig <<- function() {
+    
+   mtx <<- as.matrix(USArrests) #changed from USArrests[rows.of.interest]
+   #heatmap.2(mtx, trace="none", col=rev(heat.colors(10)), margins=c(20, 20))
+   hc1 <<- hclust(dist(mtx))
+   hc2 <<- hclust(dist(t(mtx)))
+
+   treeMtx <<- cutree(hc1, k=1:50)
+   treeMtx2 <<- cutree(hc2, k=1:4)
+
+   rowname <<- hc1$labels
+   rowclust <<- hc1$order
+   rowrank <<- hc1$order
+   hc1_rev <<- rev(hc1$order) 
+   rowgroup <<- list()
+   rowFill <<- function() {
+                  for(i in 1:nrow(treeMtx)) {
+                     rowgroup[[i]] <<- rev(treeMtx[hc1_rev[i],])
+                     }
+                  return(I(rowgroup))
+                  }
+
+   row_nodes <<- data.frame(name=rowname,
+                            clust=rowclust,
+			    rank=rowrank,
+			    group=rowFill(), #function that fills list called rowgroup with rows from treeMtx
+			    stringsAsFactors=FALSE)
+
+   colname <<- hc2$labels
+   colclust <<- hc2$order
+   colrank <<- hc2$order
+   hc2_rev <<- rev(hc2$order) 
+   colgroup <<- list()
+   colFill <<- function() {
+                  for(i in 1:nrow(treeMtx2)) {
+                     colgroup[[i]] <<- rev(treeMtx2[hc2_rev[i],])
+                     }
+                  return(I(colgroup))
+                  }
+
+   col_nodes <<- data.frame(name=colname,
+                            clust=colclust,
+			    rank=colrank,
+			    group=colFill(), #function that fills list called colgroup with rows from treeMtx2
+			    stringsAsFactors=FALSE)
+
+   mat <<- unname(mtx, force=FALSE)
+
+   dataFrameRaw <<- list(row_nodes=row_nodes, col_nodes=col_nodes, mat=mat)
+   dataFramejson <<- toJSON(dataFrameRaw)
+
+   return(dataFrameRaw)
+   #return(dataFramejson) 
+   
+   }#dataFrameBig
 #--------------------------------------------------------------------------------
